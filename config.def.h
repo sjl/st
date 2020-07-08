f333470526c9620862c98f152cbcbbfe0ab9e001
Steve Losh <steve.losh@10xgenomics.com>
2020-07-08 15:45:45 -0400

    Add my configuration

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -107,31 +107,32 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+       /* 8 normal colors */
+       "#070807",
+       "#e93f40",
+       "#88dc02",
+       "#fb7d01",
+       "#0abcfe",
+       "#fe5691",
+       "#76dff3",
+       "#f4f4f4",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+       /* 8 bright colors */
+       "#5d5d5d",
+       "#e93f40",
+       "#9df903",
+       "#fadf31",
+       "#40cdfe",
+       "#fe2b74",
+       "#09cef2",
+       "#fefffe",
 
-	[255] = 0,
+       [255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+       /* more colors can be added after 255 to use with DefaultXX */
+       "#cccccc",
+       "#555555",
+       "#1c0816",
 };
 
 
@@ -140,8 +141,8 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultbg = 258;
+static unsigned int defaultcs = 4;
 static unsigned int defaultrcs = 257;
 
 /*
@@ -199,6 +200,9 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+static char *editscrollbackcmd[] = { "/home/sjl/src/dotfiles/bin/st-edit", NULL };
+static char *browscmd[] = { "/home/sjl/src/dotfiles/bin/st-brows", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -215,6 +219,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+
+	{ TERMMOD,              XK_M,           externalpipe,   { .v = editscrollbackcmd } },
+	{ TERMMOD,              XK_U,           externalpipe,   { .v = browscmd } },
 };
 
 /*
